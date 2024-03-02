2cf5389e6ac718b88f2ddd5949529ca36d7fa5d3
rorsdev <rorsdev@gmail.com>
2024-03-02 20:00:19 +0100

    Added branch and solution for ac 2.6

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,38 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+
+  const addNewPerson = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      id: Date.now(),
+      name: newName,
+      number: ''
+    }
+    setPersons([...persons, newPerson])
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={ addNewPerson }>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={(event) => setNewName(event.target.value) } />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        { 
+          persons.map((person, i) => (
+            <li key={i}>
+              { person.name } { person.number }
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 };
