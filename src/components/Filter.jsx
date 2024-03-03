f2aabbccdbe98f501663730d9c4a5a286069c955
rorsdev <rorsdev@gmail.com>
2024-03-03 17:53:27 +0100

    Added branch and made my first handler

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,10 +1,16 @@
 export const Filter = ({ setNewFilter }) => {
 
+    const handleChangeFilterValue = (event) => {
+        setNewFilter(event.target.value)
+    }
+
     return (
         <form>
             <div>
                 <label htmlFor="filter">Filter by: </label>
-                <input htmlFor="filter" onChange={ (event) => setNewFilter(event.target.value) }/>
+                {/* <input htmlFor="filter" onChange={ (event) => handleChangeFilterValue(event) }/> */}
+                {/* Works as the line before */}
+                <input htmlFor="filter" onChange={ handleChangeFilterValue }/>
             </div>
         </form>
     );
