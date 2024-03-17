720b8eb82c50f297508e89be0674c00bcf675628
Adriana García <adrianagarciarodriguezzz@gmail.com>
2024-03-17 18:43:10 +0100

    Add json-server and db.json file, update usePersons hook

--- src/hooks/usePersons.jsx
+++ src/hooks/usePersons.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-export const usePersons = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ]);
+const usePersons = () => {
+    const [persons, setPersons] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data);
+            });
+    }, []);
 
     const handleChangePersonsValue = (newValue) => {
         setPersons(newValue)
@@ -18,3 +21,4 @@ export const usePersons = () => {
     };
 }
 
+export default usePersons;
