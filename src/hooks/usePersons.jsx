f438efe9af539af31fc176884ba3ab7c2259421a
Adriana Garcia <adrianagarciarodriguezzz@gmail.com>
2024-03-18 19:08:21 +0100

    Export usePersons hook

--- src/hooks/usePersons.jsx
+++ src/hooks/usePersons.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const usePersons = () => {
+export const usePersons = () => {
     const [persons, setPersons] = useState([]);
 
     useEffect(() => {
